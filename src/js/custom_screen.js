6b316ace0205d1b3efa4f00d31fd80e0d0d6b602
Dima Mironov <okdimok@gmail.com>
2015-07-30 14:02:41 +0200

    my bg

--- src/js/custom_screen.js
+++ src/js/custom_screen.js
@@ -1,9 +1,10 @@
-define([ 'vas/core/base/components', 'vas/core/registry' ], function( C, R ) {
+define([ 'require', 'vas/core/base/components', 'vas/core/registry' ], function( C, R ) {
    var MyBackdrop = function( hostDOM ) {
       // 2) Call parent constructor
       C.Backdrop.call(this, hostDOM);
+      hostDOM.css("background-image", "url(" + require.toUrl('../img/background.pnd') + ")");
+	  hostDOM.css("background-size", "cover");
 
-	  hostDOM.css("background-image", "url(modules/vas/basic/img/blackboard.jpg)");	
    }
    // 1) Import parent component prototype methods
    MyBackdrop.prototype = Object.create( C.Backdrop.prototype );
