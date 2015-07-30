83dd2d9468256f3e6f80c0b21a419e2db8d24b9e
Dima Mironov <okdimok@gmail.com>
2015-07-30 14:32:07 +0200

    tt6

--- src/js/custom_screen.js
+++ src/js/custom_screen.js
@@ -1,9 +1,19 @@
-define([ 'require', 'vas/core/base/components', 'vas/core/registry' ], function( require, C, R ) {
+define([ 'require', 'vas/core/base/components', 'vas/core/registry', 'text!../tpl/backdrop.html' ], function( require, C, R, Template ) {
    var MyBackdrop = function( hostDOM ) {
       // 2) Call parent constructor
       C.Backdrop.call(this, hostDOM);
-      hostDOM.css("background-image", "url(" + require.toUrl('../img/background.png') + ")");
-	  hostDOM.css("background-size", "cover");
+	  hostDOM.addClass("custom-backdrop");
+	  this.loadTemplate( Template  );
+      this.setViewData("message", "Hello world!"); 
+	  this.renderView();
+
+	  $(window).mousemove((function(e) {
+         this.select(".floating-text").css({
+            'left': e.clientX,
+            'top': e.clientY,
+         });
+      }).bind(this));
+
 
    }
    // 1) Import parent component prototype methods
